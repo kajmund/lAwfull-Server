3d23138b10887956705ef8a304b2c04c35b17143
Jonas Bergvall <jonas.bergvall@gmail.com>
2012-02-22 16:51:19 +0100

    Simple div-based layout.

--- src/main/java/lawscraper/server/service/LawRendererImpl.java
+++ src/main/java/lawscraper/server/service/LawRendererImpl.java
@@ -11,106 +11,42 @@ import java.util.List;
 public class LawRendererImpl implements LawRenderer {
     @Override
     public String renderToHtml(Law law) {
-        return element("table", element("tbody", renderMeta(law) + renderParts(law.getChildren())));
+        return element("div", renderMeta(law)
+                + element("div", renderParts(law.getChildren()), "class", "lawBody"),
+                "class", "law");
     }
 
     private String renderParts(List<LawDocumentPart> children) {
-        return renderParts(children, new LawRenderContext());
-    }
-
-    private String renderParts(List<LawDocumentPart> children, LawRenderContext context) {
         StringBuilder sb = new StringBuilder();
         for (LawDocumentPart child : children) {
-            sb.append(renderPart(child, context));
+            sb.append(renderPart(child));
         }
         return sb.toString();
     }
 
-    private String renderPart(LawDocumentPart part, LawRenderContext context) {
-        switch (part.getLawPartType()) {
-            case DIVIDER:
-                return renderDivider(part, context);
-            case CHAPTER:
-                return renderChapter(part, context);
-            case DEPRECATED:
-                return renderDeprecated(part, context);
-            case HEADING:
-                return renderHeader(part, context);
-            case PARAGRAPH:
-                return renderParagraph(part, context);
-            case SECTION:
-                return renderSection(part, context);
-            case SECTION_LIST_ITEM:
-                return renderSectionListItem(part);
-        }
-        return null;
+    private String renderPart(LawDocumentPart part) {
+        return renderPart(part, "part_" + part.getLawPartType().name().toLowerCase());
     }
 
-    private String renderSectionListItem(LawDocumentPart part) {
-        return element("li", part.getTextElement().getText());
-    }
-
-    private String renderSection(LawDocumentPart part, LawRenderContext context) {
-        String numberedList = "";
-        if (!part.getChildren().isEmpty()) {
-            numberedList = element("ul", renderChildren(part, context), "class", "part_section_list_item");
-        }
-        return element("span", part.getTextElement().getText(), "class", "part_section") + numberedList;
-    }
-
-    private String renderDeprecated(LawDocumentPart part, LawRenderContext context) {
-        return renderTopLevelPart(part, "part_deprecated") + renderChildren(part, context);
-    }
-
-    private String renderChapter(LawDocumentPart part, LawRenderContext context) {
-        return renderTopLevelPart(part, "part_chapter") + renderChildren(part, context);
-    }
-
-    private String renderDivider(LawDocumentPart part, LawRenderContext lawRenderContext) {
-        return renderTopLevelPart(part, "part_divider") + renderChildren(part, lawRenderContext);
-    }
-
-    private String renderHeader(LawDocumentPart part, LawRenderContext context) {
-        context.increaseHeaderLevel();
-        String childContent = renderChildren(part, context);
-        context.decreaseHeaderLevel();
-        return element("tr", element("td",
-                element("h" + (2 + context.getHeaderLevel()), part.getTextElement().getText(),
-                        "class", "part_heading"))) + childContent;
-    }
-
-    private String renderParagraph(LawDocumentPart part, LawRenderContext lawRenderContext) {
-        return element("tr", element("td", element("p",
-                element("span", part.getTextElement().getText(), "class", "part_paragraph") +
-                        (part.getChildren().isEmpty() ? "" : renderPart(part.getChildren().get(0), lawRenderContext)))
-                + (part.getChildren().size() > 1 ?
-                renderParts(part.getChildren().subList(1, part.getChildren().size()))
-                : "")
-        ));
-    }
-
-    private String renderTopLevelPart(LawDocumentPart part, String cssClass) {
-        return element("tr", element("td", element("h2", part.getTextElement().getText(), "class", cssClass)));
-    }
-
-    private String renderChildren(LawDocumentPart part, LawRenderContext context) {
-        context.increaseLevel();
-        String childContents = renderParts(part.getChildren(), context);
-        context.decreaseLevel();
-        return childContents;
+    private String renderPart(LawDocumentPart part, String cssClass) {
+        return element("div", part.getTextElement().getText() + renderParts(part.getChildren()), "class", cssClass);
     }
 
     private String renderMeta(Law law) {
-        return element("tr", element("td", element("h1", getLawTitle(law), "class", "lawTitle") +
-                element("dl", getMetaPart("Departement", law.getCreator())
-                        + getMetaPart("Utfärdad", law.getReleaseDate())
-                        + getMetaPart("Källa", law.getPublisher())
-                        + getMetaPart("Senast hämtad", law.getLatestFetchFromGov())
-                )));
+        return element("div", element("div", getLawTitle(law), "class", "lawTitle") +
+                getMetaPart("Departement", law.getCreator())
+                + getMetaPart("Utfärdad", law.getReleaseDate())
+                + getMetaPart("Källa", law.getPublisher())
+                + getMetaPart("Senast hämtad", law.getLatestFetchFromGov()),
+                "class", "lawMeta"
+        );
     }
 
     private String getMetaPart(String title, String content) {
-        return element("dt", title, "class", "metaTitle") + element("dd", content, "class", "metaText");
+        return element("div",
+                element("div", title, "class", "metaTitle") +
+                        element("div", content, "class", "metaText"),
+                "class", "metaHeading");
     }
 
     private String getLawTitle(Law law) {
--- src/test/java/lawscraper/server/service/LawRendererImplTest.java
+++ src/test/java/lawscraper/server/service/LawRendererImplTest.java
@@ -19,12 +19,13 @@ public class LawRendererImplTest {
 
         String html = renderer.renderToHtml(new Law());
 
-        assertEquals("<table><tbody><tr><td><h1 class=\"lawTitle\"> ()</h1><dl>" +
-                "<dt class=\"metaTitle\">Departement</dt><dd class=\"metaText\"></dd>" +
-                "<dt class=\"metaTitle\">Utfärdad</dt><dd class=\"metaText\"></dd>" +
-                "<dt class=\"metaTitle\">Källa</dt><dd class=\"metaText\"></dd>" +
-                "<dt class=\"metaTitle\">Senast hämtad</dt><dd class=\"metaText\"></dd>" +
-                "</dl></td></tr></tbody></table>"
+        assertEquals("<div class=\"law\"><div class=\"lawMeta\">" +
+                "<div class=\"lawTitle\"> ()</div>" +
+                "<div class=\"metaHeading\"><div class=\"metaTitle\">Departement</div><div class=\"metaText\"></div></div>" +
+                "<div class=\"metaHeading\"><div class=\"metaTitle\">Utfärdad</div><div class=\"metaText\"></div></div>" +
+                "<div class=\"metaHeading\"><div class=\"metaTitle\">Källa</div><div class=\"metaText\"></div></div>" +
+                "<div class=\"metaHeading\"><div class=\"metaTitle\">Senast hämtad</div><div class=\"metaText\"></div></div>" +
+                "</div></div>"
                 , html);
     }
 
@@ -38,7 +39,7 @@ public class LawRendererImplTest {
         law.setPublisher("Publisher");
         law.setReleaseDate("RelDate");
         law.setLatestFetchFromGov("FetchDate");
-        
+
         LawDocumentPart sli = getPart("SectionListItem", LawDocumentPartType.SECTION_LIST_ITEM);
         LawDocumentPart s = getPart("Section", LawDocumentPartType.SECTION);
         LawDocumentPart p = getPart("Paragraph", LawDocumentPartType.PARAGRAPH);
@@ -59,23 +60,25 @@ public class LawRendererImplTest {
 
         String html = renderer.renderToHtml(law);
 
-        assertEquals("<table><tbody><tr><td><h1 class=\"lawTitle\">Law (FS)</h1><dl>" +
-                "<dt class=\"metaTitle\">Departement</dt><dd class=\"metaText\">Creator</dd>" +
-                "<dt class=\"metaTitle\">Utfärdad</dt><dd class=\"metaText\">RelDate</dd>" +
-                "<dt class=\"metaTitle\">Källa</dt><dd class=\"metaText\">Publisher</dd>" +
-                "<dt class=\"metaTitle\">Senast hämtad</dt><dd class=\"metaText\">FetchDate</dd>" +
-                "</dl></td></tr>" +
-                "<tr><td><h2 class=\"part_divider\">Divider</h2></td></tr>" +
-                "<tr><td><h2 class=\"part_chapter\">Chapter</h2></td></tr>" +
-                "<tr><td><h2 class=\"part_heading\">Heading</h2></td></tr>" +
-                "<tr><td><h3 class=\"part_heading\">Heading2</h3></td></tr>" +
-                "<tr><td>" +
-                "<p><span class=\"part_paragraph\">Paragraph</span>" +
-                "<span class=\"part_section\">Section</span>" +
-                "<ul class=\"part_section_list_item\"><li>SectionListItem</li></ul></p>" +
-                "</td></tr>" +
-                "<tr><td><h2 class=\"part_deprecated\">Deprecated</h2></td></tr>" +
-                "</tbody></table>"
+        assertEquals("<div class=\"law\"><div class=\"lawMeta\">" +
+                "<div class=\"lawTitle\">Law (FS)</div>" +
+                "<div class=\"metaHeading\"><div class=\"metaTitle\">Departement</div><div class=\"metaText\">Creator</div></div>" +
+                "<div class=\"metaHeading\"><div class=\"metaTitle\">Utfärdad</div><div class=\"metaText\">RelDate</div></div>" +
+                "<div class=\"metaHeading\"><div class=\"metaTitle\">Källa</div><div class=\"metaText\">Publisher</div></div>" +
+                "<div class=\"metaHeading\"><div class=\"metaTitle\">Senast hämtad</div><div class=\"metaText\">FetchDate</div></div>" +
+                "</div>" +
+
+                "<div class=\"lawBody\">" +
+                "<div class=\"part_divider\">Divider" +
+                "<div class=\"part_chapter\">Chapter" +
+                "<div class=\"part_heading\">Heading" +
+                "<div class=\"part_heading\">Heading2" +
+                "<div class=\"part_paragraph\">Paragraph" +
+                "<div class=\"part_section\">Section" +
+                "<div class=\"part_section_list_item\">SectionListItem" +
+                "</div></div></div></div></div></div></div>" +
+                "<div class=\"part_deprecated\">Deprecated</div>" +
+                "</div></div>"
                 , html);
     }
 
